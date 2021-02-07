c630d8da66d80bce8985c9800ca51a604a74ef41
georgeyeint <georgeyeint@gmail.com>
2021-02-07 16:31:02 +0300

    Remove local 'disable token check'

--- app/Http/Middleware/APIAuth.php
+++ app/Http/Middleware/APIAuth.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 
 class APIAuth
 {
@@ -18,18 +17,16 @@ class APIAuth
      */
     public function handle(Request $request, Closure $next)
     {
-        if (App::environment() !== 'local') { //todo token local??
-            // API token can get from header or request
-            $apiToken = $request->header('X-Auth-Token', null);
-            $apiToken = $request->api_token ?: $apiToken;
+        // API token can get from header or request
+        $apiToken = $request->header('X-Auth-Token', null);
+        $apiToken = $request->api_token ?: $apiToken;
 
-            if ($apiToken) {
-                $userFound = User::where('api_token', '=', $apiToken)->first() ?: null;
-            }
+        if ($apiToken) {
+            $userFound = User::where('api_token', '=', $apiToken)->first() ?: null;
+        }
 
-            if (!($apiToken && $userFound)) {
-                return response('Unauthorized', 401);
-            }
+        if (!($apiToken && $userFound)) {
+            return response('Unauthorized', 401);
         }
 
         return $next($request);
