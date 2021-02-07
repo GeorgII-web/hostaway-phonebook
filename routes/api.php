2a5d4e12bfdced7f36876c20954ce3aae4edd74f
georgeyeint <georgeyeint@gmail.com>
2021-02-07 14:29:54 +0300

    Refactor rotes to resuorce

--- routes/api.php
+++ routes/api.php
@@ -14,19 +14,12 @@ use App\Http\Controllers\ItemController;
 |
 */
 
-//todo add log middleware
-//todo Route::middleware('auth:api')
-
 Route::middleware('throttle:200,1')
     ->middleware('log.route')
     ->middleware('token')
     ->group(function () {
 
-        Route::get('items', [ItemController::class, 'list']);
-        Route::get('items/{id}', [ItemController::class, 'get']);
-        Route::post('items', [ItemController::class, 'create']);
-        Route::patch('items/{id}', [ItemController::class, 'update']);
-        Route::delete('items/{id}', [ItemController::class, 'delete']);
+        Route::resource('items', ItemController::class);
 
     });
 
